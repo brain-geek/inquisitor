2017b8529a8d5da05e50f3fab99dc89baa69a454
Alex Rozumey <brain-geek@yandex.ua>
2011-12-16 15:50:36 +0200

    Simplify specs

--- spec/frontend_spec.rb
+++ spec/frontend_spec.rb
@@ -1,8 +1,6 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe "My App" do
-  include Rack::Test::Methods
-
   def app
     @app ||= Sinatra::Application
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 require File.join(File.dirname(__FILE__), '..', 'frontend.rb')
 
 require 'rubygems'
-require 'sinatra'
 require 'rack/test'
 require 'rspec'
 
@@ -10,3 +9,7 @@ set :environment, :test
 set :run, false
 set :raise_errors, true
 set :logging, false
+
+RSpec.configure do |conf|
+  conf.include Rack::Test::Methods
+end
