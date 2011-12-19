3713ba8e85a7dea47f420fa506475117619c0955
Alex Rozumey <brain-geek@yandex.ua>
2011-12-19 15:57:46 +0200

    Move models to own folder

---
+++ app/models/node.rb
@@ -0,0 +1,55 @@
+require 'uri'
+require 'outpost'
+require 'outpost/scouts'
+
+class Node
+  include DataMapper::Resource  
+  property :id,           Serial
+  property :url,          String
+  property :name,         String
+
+  validates_presence_of :url
+  validates_with_method :url, :method => :check_protocol
+
+  def check
+    outpost.run
+  end
+
+  def last_log
+    outpost.messages.join('<br />')
+  end
+
+  def check_protocol
+    outpost
+    true
+  rescue URI::InvalidURIError
+    false
+  end
+
+  protected
+  def outpost
+    @outpost ||= begin
+      outpost = Outpost::Application.new
+      uri = URI.parse(url)
+      outpost.name = self.name
+
+      case uri.scheme 
+      when 'http'
+        outpost.add_scout Outpost::Scouts::Http => '' do
+          options :host => uri.host, :port => uri.port, :path => uri.path.empty? ? '/' : uri.path
+          report :up, :response_code => 200...400
+          report :down, :response_code => 400..600
+        end
+      when 'ping'
+        outpost.add_scout Outpost::Scouts::Ping => '' do
+          options :host => uri.host
+          report :up, :response_time => {:less_than => 500}
+        end
+      else
+        raise URI::InvalidURIError
+      end
+
+      outpost
+    end
+  end
+end
