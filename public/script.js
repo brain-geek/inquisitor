74fd1f24610d1f1211ea77714fb50f33f6ca5a43
Alex Rozumey <brain-geek@yandex.ua>
2011-12-19 18:34:34 +0200

    Add images to illustrate status of service.

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ $(function(){
 	$('td.status-load').each(function(index, element){
 		$.get($(this).attr('rel'), '', function(req)
 		{
-			$('.element-' + req['id'] + ' .status-load').html(req.status)
+			$('.element-' + req['id'] + ' .status-load').html('<img src="' + req.status + '.png" />')
 			$('.element-' + req['id'] + ' .status-load').attr('detailed', req.log)
 		});
 	});
