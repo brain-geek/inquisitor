fe4b227dfd4490a5b2718e4bca097f9d7075f614
Alex Rozumey <brain-geek@yandex.ua>
2011-12-20 12:18:22 +0200

    Move environment to app itself

--- frontend.rb
+++ frontend.rb
@@ -30,6 +30,38 @@ class Monitor < Sinatra::Base
 
     redirect '/'
   end 
-end
 
-require './config/environments'
+  # set test environment
+  configure :test do
+    if ENV['DB'] == 'sqlite'
+      DataMapper.setup(:default, 'sqlite::memory:')
+    elsif ENV['DB'] == 'redis'
+      DataMapper.setup(:default, {:adapter  => "redis"})      
+    else
+      DataMapper.setup(:default, 'sqlite::memory:')
+      puts 'Using sqlite by default!'
+    end
+    DataMapper.auto_upgrade!
+    set :run, false
+    set :raise_errors, true
+    set :logging, false
+  end
+
+  #set development environment
+  configure :development do
+    DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+    DataMapper.auto_upgrade!    
+    set :run, false
+    set :raise_errors, true
+    set :logging, true    
+  end
+
+  #set development environment
+  configure :production do
+    DataMapper.setup(:default, settings.database)
+    DataMapper.auto_upgrade! 
+    set :run, false
+    set :raise_errors, false
+    set :logging, false
+  end  
+end
